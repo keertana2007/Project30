96503a3aa99cfa9d8278d6e5c81b683fbdc5e30c
keertana2007 <74975297+keertana2007@users.noreply.github.com>
2021-01-14 20:11:10 +0530

    Update box2.js

--- box2.js
+++ box2.js
@@ -19,7 +19,7 @@ class Box_2 {
         translate(pos.x, pos.y);
         rotate(angle);
         rectMode(CENTER);
-        fill("red");
+        fill("magenta");
         rect(0,0, this.width, this.height);
       }
       else{
